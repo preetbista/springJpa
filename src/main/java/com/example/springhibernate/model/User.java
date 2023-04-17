f798327c15c5ce19608cab8db43d62afa55d379c
preetbista <bistapreet4@gmail.com>
2023-04-17 11:01:03 +0545

    Setup only

---
+++ src/main/java/com/example/springhibernate/model/User.java
@@ -0,0 +1,39 @@
+package com.example.springhibernate.model;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String role;
+
+    protected User(){
+
+    }
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
